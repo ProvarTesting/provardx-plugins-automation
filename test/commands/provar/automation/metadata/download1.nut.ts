3144a5e2bf5f4a13fa41a86e1e4764da6b664920
AnchalGoel <46100479+AnchalGoel@users.noreply.github.com>
2024-05-15 19:12:19 +0530

    PDX-283 download1 nuts changes

--- test/commands/provar/automation/metadata/download1.nut.ts
+++ test/commands/provar/automation/metadata/download1.nut.ts
@@ -10,7 +10,7 @@ import { TestSession } from '@salesforce/cli-plugins-testkit';
 // import * as metadataDownloadConstants from '../../../../assertion/metadataDownloadConstants.js';
 // mport { config } from '@oclif/core/lib/errors/config.js';
 import { Global } from '@salesforce/core';
-import { ProvarConfig } from '@provartesting/provardx-plugins-utils';
+// import { ProvarConfig } from '@provartesting/provardx-plugins-utils';
 
 describe('setting Path at sf config path location', () => {
   let session: TestSession;
@@ -20,11 +20,34 @@ describe('setting Path at sf config path location', () => {
   });
 
   it('It should update the PROVARDX_PROPERTIES_FILE_PATH in sfdx config.json', () => {
+    async function UpdateFileConfigSfdx() {
+      try {
+        const file = (await fileSystem.readdir(Global.SFDX_DIR)).find((filename: string) =>
+          filename.match('.*config.json')
+        );
+        const fileData = await fileSystem.readFile(path.resolve(file ? `${Global.SFDX_DIR}\\${file}` : ''), {
+          encoding: 'utf8',
+        });
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const configFile = JSON.parse(fileData);
+
+        const configFilePath = path.join(process.cwd(), './dummy.json');
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
+        await fileSystem.writeFile(configFile, JSON.stringify(configFilePath, null, 4), 'utf8');
+        // eslint-disable-next-line no-console
+        console.log('Config file updated successfully.');
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error('Error on updating the config file', error);
+      }
+    }
+    void UpdateFileConfigSfdx();
+
     // type Config = {
     //   PROVARDX_PROPERTIES_FILE_PATH?: string;
     // };
     // async function UpdateSfdxConfig() {
-    //   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
     //   const configPath = path.join(os.homedir(), '.sfdx');
     //   if (!fileSystem.existsSync(configPath)) {
     //     fileSystem.mkdirSync(configPath, { recursive: true });
@@ -50,36 +73,12 @@ describe('setting Path at sf config path location', () => {
     // }
     // // eslint-disable-next-line @typescript-eslint/no-floating-promises
     // void UpdateSfdxConfig();
-
-    async function UpdateFileConfigSfdx() {
-      try {
-        const file = (await fileSystem.readdir(Global.SFDX_DIR)).find((filename: string) =>
-          filename.match('.*config.json')
-        );
-        const fileData = await fileSystem.readFile(path.resolve(file ? `${Global.SFDX_DIR}\\${file}` : ''), {
-          encoding: 'utf8',
-        });
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-        const configFile = JSON.parse(fileData);
-
-        const configFilePath = path.join(process.cwd(), './dummy.json');
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unused-vars, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-argument
-        await fileSystem.writeFile(configFile, JSON.stringify(configFilePath, null, 4), 'utf8');
-        // eslint-disable-next-line no-console
-        console.log('Config file updated successfully.');
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error('Error on updating the config file', error);
-      }
-    }
-    void UpdateFileConfigSfdx();
   });
 
   it('It should update the PROVARDX_PROPERTIES_FILE_PATH in sfdx config.json', async () => {
     //   type config = {
     //     PROVARDX_PROPERTIES_FILE_PATH?: string;
     // }
-
     // const propertiesFileName = path.join(process.cwd(), './provardx-properties.json');
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     // const config: ProvarConfig = await ProvarConfig.loadConfig(this.errorHandler);
