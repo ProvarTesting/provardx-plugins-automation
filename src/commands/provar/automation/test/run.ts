49242b92ffa374fef622aabb9e85977e91326340
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-07-26 19:11:33 +0530

    PDX-376: revert changes
    RCA: commited the fix in wrong release ebranch so reverting it
    
    Fix: removing fix done wrt other version 1.0.2

--- src/commands/provar/automation/test/run.ts
+++ src/commands/provar/automation/test/run.ts
@@ -46,24 +46,14 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
       const propertiesData = fileSystem.readFileSync(propertiesFilePath, { encoding: 'utf8' });
       const propertiesInstance = JSON.parse(propertiesData);
       if (propertiesInstance?.testCase) {
-        propertiesInstance.testCase = propertiesInstance.testCase.map((testCasePath: string) => {
-          if (testCasePath.startsWith('/tests')) {
-            return getStringAfterSubstring(testCasePath, '/tests');
-          } else if (testCasePath.startsWith('tests')) {
-            return getStringAfterSubstring(testCasePath, 'tests');
-          }
-          return testCasePath;
-        });
+        propertiesInstance.testCase = propertiesInstance.testCase.map((testCasePath: string) =>
+          testCasePath.replace(/^\/tests\//, '/')
+        );
       }
       if (propertiesInstance?.testPlan) {
-        propertiesInstance.testPlan = propertiesInstance.testPlan.map((testCasePathInstance: string) => {
-          if (testCasePathInstance.startsWith('/plans')) {
-            return getStringAfterSubstring(testCasePathInstance, '/plans');
-          } else if (testCasePathInstance.startsWith('plans')) {
-            return getStringAfterSubstring(testCasePathInstance, 'plans');
-          }
-          return testCasePathInstance;
-        });
+        propertiesInstance.testPlan = propertiesInstance.testPlan.map((testCaseInstancePath: string) =>
+          testCaseInstancePath.replace(/^\/plans\//, '/')
+        );
       }
       const rawProperties = JSON.stringify(propertiesInstance);
       const userSupport = new UserSupport();
