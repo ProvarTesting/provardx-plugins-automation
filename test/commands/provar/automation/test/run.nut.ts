b025e3a5f7ae04d60df65b66a23bcb5177b800b5
Priya Ranjan <priya.ranjan@provartesting.com>
2024-08-08 20:42:08 +0530

    PDX-283: Path.join function for Global.SF_DIR was failing on Pipeline in run.nut.ts so updated the function
    RCA: Ptah.join function for Global.SF_DIR was failing on Pipeline
    
    Fix: Path.join function for Global.SF_DIR updated

--- test/commands/provar/automation/test/run.nut.ts
+++ test/commands/provar/automation/test/run.nut.ts
@@ -18,19 +18,11 @@ describe('provar automation test run NUTs', () => {
   let configFilePath = '';
 
   async function UpdateFileConfigSfdx(): Promise<void> {
-    try {
-      const files = await fs.readdir(Global.SF_DIR);
-      const configFileName = files.find((filename) => filename.match('.*config.json'));
+    const files = await fs.readdir(Global.SF_DIR);
+    const configFileName = files.find((filename) => filename.match('.*config.json'));
 
-      if (!configFileName) {
-        throw new Error('config.json file not found');
-      }
-
-      // configFilePath = path.join(Global.SF_DIR, configFileName);
-      configFilePath = path.join(`${Global.SF_DIR}`, `${configFileName}`);
-    } catch (error) {
-      throw new Error('Error on updating the config file');
-    }
+    // configFilePath = path.join(Global.SF_DIR, configFileName);
+    configFilePath = path.join(`${Global.SF_DIR}`, `${configFileName}`);
   }
   enum FILE_PATHS {
     PROVARDX_PROPERTIES_FILE = 'provardx-properties.json',
