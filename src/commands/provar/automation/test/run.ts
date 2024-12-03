f4be1dbfb8cca5ccc8055193dd35fe3f7d385eda
Palak-Bansal-Provar <89642292+Palak-Bansal-Provar@users.noreply.github.com>
2024-12-03 14:02:21 +0530

    PDX-410: Show test run output on terminal by default (#48)
    sf provar automation test run
    outputs the test run logs on the terminal by default
    outputs the test run logs to a file when the -o | --output-file param is used, with the file name as specified
    Changes:- have added a check for output file flag before writing to the file.
    Have used the same mechanism as earlier that is piping the child process output , instead of directly using execSync for logging the output to console, as in case of logging to the console we also need consolidated results which is only possible if we are storing the logs anywhere in the code.

--- src/commands/provar/automation/test/run.ts
+++ src/commands/provar/automation/test/run.ts
@@ -1,6 +1,7 @@
 import * as fileSystem from 'node:fs';
 import { spawn } from 'node:child_process';
-import { SfCommand } from '@salesforce/sf-plugins-core';
+import * as path from 'node:path';
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
 import { xml2json } from 'xml-js';
 import {
   sfCommandConstants,
@@ -13,9 +14,9 @@ import {
   TestRunError,
   GenericError,
   SfProvarCommandResult,
-  populateResult,
   Messages,
 } from '@provartesting/provardx-plugins-utils';
+import { SfProvarAutomationTestRunResult } from '../../../../result/sfProvarAutomationTestRunResult.js';
 
 Messages.importMessagesDirectoryFromMetaUrl(import.meta.url);
 const mdFile: string = 'provar.automation.test.run';
@@ -26,19 +27,31 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
 
+  public static readonly flags = {
+    'output-file': Flags.string({
+      summary: messages.getMessage('flags.output-file.summary'),
+      char: 'o',
+    }),
+  };
   private genericErrorHandler: GenericErrorHandler = new GenericErrorHandler();
 
   public async run(): Promise<SfProvarCommandResult> {
     const { flags } = await this.parse(ProvarAutomationTestRun);
     const config: ProvarConfig = await ProvarConfig.loadConfig(this.genericErrorHandler);
     const propertiesFilePath = config.get('PROVARDX_PROPERTIES_FILE_PATH')?.toString();
+    const sfProvarAutomationTestRunResult = new SfProvarAutomationTestRunResult();
 
     if (propertiesFilePath === undefined || !fileSystem.existsSync(propertiesFilePath)) {
       const errorObj: GenericError = new GenericError();
       errorObj.setCode('MISSING_FILE');
       errorObj.setMessage(errorMessages.MISSING_FILE_ERROR);
       this.genericErrorHandler.addErrorsToList(errorObj);
-      return populateResult(flags, this.genericErrorHandler, messages, this.log.bind(this));
+      return sfProvarAutomationTestRunResult.populateResult(
+        flags,
+        this.genericErrorHandler,
+        messages,
+        this.log.bind(this)
+      );
     }
 
     try {
@@ -73,7 +86,12 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
         this.genericErrorHandler
       );
       if (userInfo === null) {
-        return populateResult(flags, this.genericErrorHandler, messages, this.log.bind(this));
+        return sfProvarAutomationTestRunResult.populateResult(
+          flags,
+          this.genericErrorHandler,
+          messages,
+          this.log.bind(this)
+        );
       }
       const userInfoString = userSupport.prepareRawProperties(JSON.stringify({ dxUsers: userInfo }));
       const projectPath = propertiesInstance.projectPath;
@@ -82,9 +100,13 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
         errorObj.setCode('INVALID_PATH');
         errorObj.setMessage('projectPath does not exist');
         this.genericErrorHandler.addErrorsToList(errorObj);
-        return populateResult(flags, this.genericErrorHandler, messages, this.log.bind(this));
+        return sfProvarAutomationTestRunResult.populateResult(
+          flags,
+          this.genericErrorHandler,
+          messages,
+          this.log.bind(this)
+        );
       }
-      const logFilePath = projectPath + '/log.txt';
 
       const provarDxJarPath = propertiesInstance.provarHome + '/provardx/provardx.jar';
       const testRunCommand =
@@ -97,7 +119,7 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
         userInfoString +
         ' Runtests';
 
-      await this.runJavaCommand(testRunCommand, logFilePath);
+      await this.runJavaCommand(testRunCommand, flags['output-file'] ?? '');
     } catch (error: any) {
       if (error.name === 'SyntaxError') {
         const errorObj: GenericError = new GenericError();
@@ -105,7 +127,12 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
         errorObj.setMessage(errorMessages.MALFORMED_FILE_ERROR);
         this.genericErrorHandler.addErrorsToList(errorObj);
       } else if (error.name === 'MultipleFailureError') {
-        return populateResult(flags, this.genericErrorHandler, messages, this.log.bind(this));
+        return sfProvarAutomationTestRunResult.populateResult(
+          flags,
+          this.genericErrorHandler,
+          messages,
+          this.log.bind(this)
+        );
       } else {
         const errorObj: GenericError = new GenericError();
         errorObj.setCode('GENERIC_ERROR');
@@ -113,7 +140,12 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
         this.genericErrorHandler.addErrorsToList(errorObj);
       }
     }
-    return populateResult(flags, this.genericErrorHandler, messages, this.log.bind(this));
+    return sfProvarAutomationTestRunResult.populateResult(
+      flags,
+      this.genericErrorHandler,
+      messages,
+      this.log.bind(this)
+    );
   }
 
   private async runJavaCommand(command: string, logFilePath: string): Promise<void> {
@@ -125,24 +157,28 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
       resolvers.done = resolve;
       resolvers.error = error;
     });
-    const javaProcessOutput = spawn(command, { shell: true, stdio: ['pipe', 'pipe', 'pipe'] });
+    try {
+      const javaProcessOutput = spawn(command, { shell: true, stdio: ['pipe', 'pipe', 'pipe'] });
 
-    javaProcessOutput.stdout.on('data', (data: { toString: () => string }) => {
-      const logMessage = data.toString().trim();
-      this.extractReportAndAddFailuresToErrorHandler(logMessage, logFilePath);
-    });
-    javaProcessOutput.stderr.on('error', (error: { toString: () => string }) => {
-      const logError = error.toString().trim();
-      this.extractReportAndAddFailuresToErrorHandler(logError, logFilePath);
-    });
-    javaProcessOutput.stderr.on('data', (error: { toString: () => string }) => {
-      const logError = error.toString().trim();
-      this.extractReportAndAddFailuresToErrorHandler(logError, logFilePath);
-    });
+      javaProcessOutput.stdout.on('data', (data: { toString: () => string }) => {
+        const logMessage = data.toString().trim();
+        this.extractReportAndAddFailuresToErrorHandler(logMessage, logFilePath);
+      });
+      javaProcessOutput.stderr.on('error', (error: { toString: () => string }) => {
+        const logError = error.toString().trim();
+        this.extractReportAndAddFailuresToErrorHandler(logError, logFilePath);
+      });
+      javaProcessOutput.stderr.on('data', (error: { toString: () => string }) => {
+        const logError = error.toString().trim();
+        this.extractReportAndAddFailuresToErrorHandler(logError, logFilePath);
+      });
 
-    javaProcessOutput.stderr.on('finish', (error: { toString: () => string }) => {
-      resolvers.done();
-    });
+      javaProcessOutput.stderr.on('finish', (error: { toString: () => string }) => {
+        resolvers.done();
+      });
+    } catch (error: any) {
+      return;
+    }
     return promise;
   }
 
@@ -157,7 +193,27 @@ export default class ProvarAutomationTestRun extends SfCommand<SfProvarCommandRe
       errorObj.setMessage(`Error ${getStringAfterSubstring(logMessage, 'Error')}`);
       this.genericErrorHandler.addErrorsToList(errorObj);
     }
-    fileSystem.appendFileSync(logFilePath, logMessage, { encoding: 'utf-8' });
+
+    if (logFilePath) {
+      try {
+        fileSystem.appendFileSync(path.resolve(logFilePath), logMessage, { encoding: 'utf-8' });
+      } catch (error: any) {
+        if (error.code === 'ENOENT') {
+          const error: GenericError = new GenericError();
+          error.setCode('INVALID_PATH');
+          error.setMessage(`The provided output file path does not exist or is invalid.`);
+          this.genericErrorHandler.addErrorsToList(error);
+        } else if (error.code === 'EPERM' || error.code === 'EACCES') {
+          const error: GenericError = new GenericError();
+          error.setCode('INSUFFICIENT_PERMISSIONS');
+          error.setMessage('The user does not have permissions to create the output file.');
+          this.genericErrorHandler.addErrorsToList(error);
+        }
+        throw error;
+      }
+    } else {
+      this.log(logMessage);
+    }
   }
 
   private getFailureMessagesFromXML(filePath: string): void {
--- test/commands/provar/automation/test/run.nut.ts
+++ test/commands/provar/automation/test/run.nut.ts
@@ -76,7 +76,51 @@ describe('provar automation test run NUTs', () => {
     const result = execCmd<SfProvarCommandResult>(
       `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND}`
     ).shellOutput;
-    expect(result.stdout).to.deep.equal(runConstants.successMessage);
+
+    expect(result.stdout).to.deep.contains(runConstants.successMessage);
+    runConstants.successfulResultSubstrings.forEach((resultSubstring) => {
+      expect(result.stdout + result.stderr).to.include(resultSubstring);
+    });
+  });
+
+  it('Test Run command should be successful in outputfile', async () => {
+    /* eslint-disable */
+    const configFilePatheData = fileSystem.readFileSync(configFilePath, { encoding: 'utf8' });
+    const configFilePathParsed = JSON.parse(configFilePatheData);
+    configFilePathParsed['PROVARDX_PROPERTIES_FILE_PATH'] = path.join(process.cwd(), './provardx-properties.json');
+    const updatedConfigFileData = JSON.stringify(configFilePathParsed, null, 4);
+    fileSystem.writeFileSync(configFilePath, updatedConfigFileData, 'utf8');
+    const SET_PROVAR_HOME_VALUE = path.join(process.cwd(), './ProvarHome').replace(/\\/g, '/');
+    const SET_PROJECT_PATH_VALUE = path.join(process.cwd(), './ProvarRegression/AutomationRevamp').replace(/\\/g, '/');
+    const SET_RESULT_PATH = './';
+    interface PropertyFileJsonData {
+      [key: string]: string | boolean | number | string[];
+    }
+    const jsonDataString = fileSystem.readFileSync(jsonFilePath, 'utf-8');
+    const jsonData: PropertyFileJsonData = JSON.parse(jsonDataString) as PropertyFileJsonData;
+    jsonData.provarHome = SET_PROVAR_HOME_VALUE;
+    jsonData.projectPath = SET_PROJECT_PATH_VALUE;
+    jsonData.resultsPath = SET_RESULT_PATH;
+    jsonData.testCase = ['/Test Case 5.testcase'];
+    const updatedJsonDataString = JSON.stringify(jsonData, null, 2);
+    fileSystem.writeFileSync(jsonFilePath, updatedJsonDataString, 'utf-8');
+    const outputFile = runConstants.outputFileAtRelativeLocation;
+    let filePath = '';
+    const result = execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND} -o ${outputFile}`
+    ).shellOutput;
+    expect(result.stdout).to.deep.contains('The test results are stored in ' + outputFile + '\n');
+    if (!outputFile.match('/')) {
+      filePath = path.join(process.cwd(), 'result.txt');
+    } else if (outputFile.startsWith('.')) {
+      filePath = path.join(process.cwd(), outputFile.replace(outputFile.charAt(0), ''));
+    } else {
+      filePath = outputFile;
+    }
+    const fileContent = fileSystem.readFileSync(filePath, 'utf-8');
+    runConstants.successfulResultSubstrings.forEach((resultSubstring) => {
+      expect(fileContent).to.include(resultSubstring);
+    });
   });
   it('Test Run command should be successful and return result in json', () => {
     const result = execCmd<SfProvarCommandResult>(
@@ -84,6 +128,15 @@ describe('provar automation test run NUTs', () => {
     ).jsonOutput;
     expect(result).to.deep.equal(runConstants.SuccessJson);
   });
+
+  it('Test Run command should be successful in case of outfile and return result in json', () => {
+    const outputFile = runConstants.outputFileAtRelativeLocation;
+    const result = execCmd<SfProvarCommandResult>(
+      `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND} -o ${outputFile} --json`
+    ).jsonOutput;
+    expect(result).to.deep.equal(runConstants.SuccessJson);
+  });
+
   it('Test Run command should not be successful and return the error', () => {
     interface PropertyFileJsonData {
       [key: string]: string | boolean | number | string[];
@@ -97,7 +150,10 @@ describe('provar automation test run NUTs', () => {
     const result = execCmd<SfProvarCommandResult>(
       `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND}`
     ).shellOutput;
-    expect(result.stderr).to.deep.equal(runConstants.errorMessage);
+    expect(result.stderr).to.deep.contains(runConstants.errorMessage);
+    runConstants.successfulResultSubstrings.forEach((resultSubstring) => {
+      expect(result.stdout + result.stderr).to.include(resultSubstring);
+    });
   });
 
   it('Test Run command should not be successful and return result in json format', () => {
@@ -134,7 +190,10 @@ describe('provar automation test run NUTs', () => {
     const result = execCmd<SfProvarCommandResult>(
       `${commandConstants.SF_PROVAR_AUTOMATION_TEST_RUN_COMMAND}`
     ).shellOutput;
-    expect(result.stderr).to.deep.equal(runConstants.errorMessage);
+    expect(result.stderr).to.deep.contains(runConstants.errorMessage);
+    runConstants.successfulResultSubstrings.forEach((resultSubstring) => {
+      expect(result.stdout + result.stderr).to.include(resultSubstring);
+    });
   });
 
   it('Test case should be Executed successfully when Environment is encrypted and return result in json format', () => {
